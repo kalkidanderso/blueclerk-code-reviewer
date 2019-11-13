f9d9f1cfe05f267493d16a8718cc09fbd30f08db
Francisco <francisco.r.ochoa@gmail.com>
2019-11-13 13:17:17 -0600

    Change root route msg

--- src/server.ts
+++ src/server.ts
@@ -61,7 +61,7 @@ app.use(logger('dev'))
 app.use(
     '/',
     function(req, res){
-       return res.json({msg: "test success"})
+       return res.json({msg: "test success 01"})
     })
 
 //Swagger
