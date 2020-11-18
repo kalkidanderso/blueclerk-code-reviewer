28f582df6acaee3572fe65b3bd974a72258bf8d5
ivanmora <iam.ivan.mora@gmail.com>
2020-11-17 21:44:09 -0600

    [BLUECLERK-80] - Temp rename for file casing correction

--- src/routes/jobSite.ts
+++ src/routes/jobSite.ts
@@ -1,7 +1,6 @@
 import express from 'express'
 import passport from 'passport'
 
-import { getCompanyId } from '../middleware/company'
 import { create, update, get } from '../controllers/jobSite'
 
 const router: express.Router = express.Router()
