eb5d868cbe1082578c70091abb1ef2c124c2eea6
Amar <medaamarnadh@gmail.com>
2021-02-09 01:02:57 +0530

    Added contact to the Job Location

--- src/controllers/contact.ts
+++ src/controllers/contact.ts
@@ -34,13 +34,23 @@ const createContactForCustomer = async (data: any, customer: ICustomer) => {
 
 
 const createContactForJobLocation = async (data: any, jobLocationId: String) => {
+    const customer = await Customer.findOne({jobLocations: jobLocationId})
+    const contact = await createContactForCustomer(data, customer)
+    await JobLocation.findByIdAndUpdate(jobLocationId, {$push:{contacts: contact._id}}, {new: true})
+    return contact
+}
 
+const addContactToTheJobLocation = async (contactId: string, jobLocationId: string) => {
+    await JobLocation.findByIdAndUpdate(jobLocationId, {$push:{contacts: contactId}})
+    const contact = await Contact.findById(contactId)
+    return contact
 }
 
 
 
 export const addContact = async (req: Request, res: Response) => {
     try {
+        let contact = null
         if(req.body.type === 'Customer') {
             const customer = await Customer.findOne({_id: req.body.referenceNumber})
             if(!customer) {
@@ -49,7 +59,19 @@ export const addContact = async (req: Request, res: Response) => {
             const result = await createContactForCustomer({name: req.body.name, phone: req.body.phone, email: req.body.email}, customer)
             return res.json({'status': Status.Created, contact: result})
         } else if(req.body.type === 'JobLocation'){
-            //TODO: Yet to implement.
+            const jobLocation = await JobLocation.findOne({_id: req.body.referenceNumber})
+            console.log('Job location::::::')
+            console.log(jobLocation)
+            if(!jobLocation) {
+                return res.json({ status: Status.Error, message: 'Job location not found'})
+            }
+            if(req.body.contactId) {
+                contact = await addContactToTheJobLocation(req.body.contactId, req.body.referenceNumber)
+            } else {
+                console.log('Before going to the create contact for the job location::::::::')
+                contact = await createContactForJobLocation({name: req.body.name, email: req.body.email, phone: req.body.phone}, jobLocation._id)
+            }
+            return res.json({success: Status.Created, contact})
         }
     } catch (err) {
         console.log(err)
@@ -87,7 +109,13 @@ export const getContacts = async (req: Request, res: Response) => {
             })
             
         } else {
-
+            JobLocation.findOne({_id: req.query.referenceNumber}).populate({ path : 'contacts'}).exec((err: any, customer: ICustomer)=> {
+                if (customer) {
+                    return res.json({status: Status.Success, result: customer.contacts})
+                } else {
+                    return res.json({ status: Status.Error, message: 'Customer not found'})
+                }
+            })
         }
     } catch (err) {
         console.log(err)
