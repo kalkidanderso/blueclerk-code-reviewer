7dd5a2830ecb0fe326ce806a4200e4fe74f52b86
anonymmed <mohamed.abdelhafidh94@gmail.com>
2021-02-16 02:35:27 +0100

    updating jobs api

--- src/controllers/job.ts
+++ src/controllers/job.ts
@@ -48,7 +48,7 @@ export const createJob = (req: Request, res: Response) => {
             }
 
             return new Promise((resolve, reject) => {
-            
+
                 Job.findOne( { $or: [ { company: req.companyId, technician:params.technicianId, scheduleDate: new Date(params.scheduleDate), scheduledStartTime: { $lte: newStartTime} , scheduledEndTime: { $gte: newStartTime } },
                      { company: req.companyId, technician:params.technicianId, scheduleDate: new Date(params.scheduleDate), scheduledStartTime: { $lte: newEndTime} , scheduledEndTime: { $gte: newEndTime }} ] }, (err: any, job: IJob) => {
                     if(job != undefined && job != null) {
@@ -61,12 +61,12 @@ export const createJob = (req: Request, res: Response) => {
         }else{
             return [jobId, serviceTicket]
         }
-        
+
     })
     .then((response: any) =>{
         const jobId = response[0]
         const serviceTicket = response[1]
-        
+
         _createJob(req, res, jobId, serviceTicket, (req: Request, res: Response, err: any, newJob: IJob) => {
             if(err != null){
                 return res.json({'status': Status.Error, 'message': err})
@@ -89,10 +89,10 @@ export const createJob = (req: Request, res: Response) => {
 const _createJob = (req: Request, res: Response, jobId: string, serviceTicket: IServiceTicket, next: (req: Request,res: Response, err: any, job: IJob) => void) => {
 
     const params = req.body
-    
+
     const user = <IUser>req.user
     var companyId = req.companyId;
-    
+
     if(req.otherCompanyId != undefined) {
         companyId = req.otherCompanyId
     }
@@ -120,7 +120,7 @@ const _createJob = (req: Request, res: Response, jobId: string, serviceTicket: I
         var date = new Date(params.scheduleDate)
         newStartTime = new Date(date.getFullYear()+'-'+(date.getMonth()+1) +'-'+date.getDate()+' '+params.scheduledStartTime)
         job. scheduledStartTime = newStartTime
-        
+
     }
     if(params.scheduledStartTime){
         var date = new Date(params.scheduleDate)
@@ -135,18 +135,18 @@ const _createJob = (req: Request, res: Response, jobId: string, serviceTicket: I
         if (err) {
             return next(req, res, Messages.GenericError, null )
         }
-        
-        serviceTicket.updateOne({jobCreated: true}, (serviceTicketError: any, raw: any) => { 
+
+        serviceTicket.updateOne({jobCreated: true}, (serviceTicketError: any, raw: any) => {
             if (serviceTicketError) {
                 return next(req, res, Messages.GenericError, null )
             }
 
-            _sendJobEmails(req, res, job, (req: Request, res: Response, newJob: IJob) => { 
+            _sendJobEmails(req, res, job, (req: Request, res: Response, newJob: IJob) => {
                 return next(req, res, null, newJob)
             } )
 
         })
-    })    
+    })
 }
 
 
@@ -182,10 +182,10 @@ const _sendJobEmails = (req: Request, res: Response, jobCreated: IJob, next: (re
         if (err) {
             return res.json({ 'status': Status.Error, 'message': Messages.GenericError })
         }
-        
+
         var tech : any;
         var contractor : any;
-        var assigneeName: any; 
+        var assigneeName: any;
         var cust : any= job.customer
         var type : any= job.type
         var creator : any = job.createdBy
@@ -202,16 +202,16 @@ const _sendJobEmails = (req: Request, res: Response, jobCreated: IJob, next: (re
             sendJobEmailToAssignee({to: tech.auth.email, assigneeName: assigneeName, companyName: company.info.companyName, customerName: cust.profile.displayName, jobType: type.title, notes: job.description, dateTime: job.scheduleDate})
         }
         sendJobEmailToCustomer({to: cust.info.email, assigneeName: assigneeName, companyName: company.info.companyName, customerName: cust.profile.displayName, jobType: type.title, notes: job.description, dateTime: job.scheduleDate})
-        
+
         if(params.employeeType == 1) {
             sendJobEmailToAssignee({to: contractor.info.companyEmail, assigneeName: assigneeName, companyName: company.info.companyName, customerName: cust.profile.displayName, jobType: type.title, notes: job.description, dateTime: job.scheduleDate})
             sendJobEmailToCompanyAdmin({to: company.info.companyEmail, assigneeName: assigneeName, companyName: company.info.companyName, customerName: cust.profile.displayName, jobType: type.title, notes: job.description, dateTime: job.scheduleDate, vendorName: creator.profile.displayName})
         }
         next(req, res, jobCreated)
-        return 
+        return
 
     })
-    
+
 }
 
 
@@ -223,10 +223,7 @@ export const getJobs = (req: Request, res: Response) => {
     }
 
     Job.find({ company: companyId })
-        .populate({
-            path: 'ticket',
-            select: 'scheduleDateTime note ticketId'
-        })
+        .populate('ticket')
         .populate({
             path: 'technician',
             select: 'profile.displayName'
@@ -265,7 +262,7 @@ export const getJobs = (req: Request, res: Response) => {
                 return res.json({'status': Status.Error, 'message': Messages.GenericError})
             }
 
-            return res.json({'status': Status.Success, 'jobs': jobs})    
+            return res.json({'status': Status.Success, 'jobs': jobs})
         }
     )
 
@@ -274,7 +271,7 @@ export const getJobs = (req: Request, res: Response) => {
 export const getJobsByTechnicianId = (req: Request, res: Response) => {
 
     const params = req.body
-    
+
     Job.find({ technician: params.employeeId })
         .populate({
             path: 'ticket',
@@ -305,7 +302,7 @@ export const getJobsByTechnicianId = (req: Request, res: Response) => {
                 return res.json({'status': Status.Error, 'message': Messages.GenericError})
             }
 
-            return res.json({'status': Status.Success, 'jobs': jobs})    
+            return res.json({'status': Status.Success, 'jobs': jobs})
 
         }
     )
@@ -343,20 +340,20 @@ export const updateJob = (req: Request, res: Response) => {
     .then((result: any) => {
         const job = result[0]
         const item = result[1]
-        
+
         let timeSpent: number = 0
         let newcharges:  number = 0
         if(item != null && item.charges > 0) {
-            
+
             if(params.status == 2 && !item.isFixed) {
                 let starting: any = new Date(job.startTime)
                 let ending: any = Date.now()
-                
+
                 let diffMs = ( ending - starting);
                 let interval = 15 * 60 * 1000;
                 timeSpent = ((Math.ceil(diffMs / interval)*interval)/60000)/60
                 newcharges = item.charges * timeSpent
-            
+
             } else if(params.status == 2 && item.isFixed) {
                 newcharges = item.charges
             }
@@ -379,7 +376,7 @@ export const updateJob = (req: Request, res: Response) => {
         }
 
         return job.updateOne(data)
-        
+
     })
     .then((response: any) => {
         return res.json({'status': Status.Success, 'message': 'Job updated successfully.'})
@@ -398,7 +395,7 @@ export const updateJob = (req: Request, res: Response) => {
 export const startJob = (req: Request, res: Response) => {
 
     const params = req.body
-    
+
     var companyId = req.companyId;
     if(req.otherCompanyId != undefined) {
         companyId = req.otherCompanyId
@@ -419,19 +416,19 @@ export const startJob = (req: Request, res: Response) => {
             if(job.status == JobStatus.FINISHED) {
                 return res.json({'status': Status.Error, 'message': "You can't start this job, it is already finished"})
             }
-            
+
             if(job.status == JobStatus.CANCELED) {
                 return res.json({'status': Status.Error, 'message': "You can't start this job, it is already canceled"})
             }
-            
+
             job.updateOne(
                 {status: JobStatus.STARTED, startTime: Date.now()},
                 (err: any, raw: any)=> {
-                    
+
                     if (err) {
                         return res.json({'status': Status.Error, 'message': Messages.GenericError})
                     }
-    
+
                     return res.json({'status': Status.Success, 'message': 'Job started successfully.'})
                 }
             )
@@ -455,7 +452,7 @@ export const editJob = (req: Request, res: Response) => {
             if (err) {
                 return res.json({'status': Status.Error, 'message': Messages.GenericError})
             }
-            
+
             if (job == undefined || job == null) {
                 return res.json({'status': Status.Error, 'message': "Invalid job id"})
             }
@@ -465,14 +462,14 @@ export const editJob = (req: Request, res: Response) => {
             }
             job.technician = params.technicianId
             job.scheduleDate = params.scheduleDate
-            
+
             let newStartTime: any = null
             let newEndTime: any = null
             if(params.scheduledStartTime){
                 var date = new Date(params.scheduleDate)
                 newStartTime = new Date(date.getFullYear()+'-'+(date.getMonth()+1) +'-'+date.getDate()+' '+params.scheduledStartTime)
                 job. scheduledStartTime = newStartTime
-                
+
             }
             if(params.scheduledStartTime){
                 var date = new Date(params.scheduleDate)
@@ -491,15 +488,15 @@ export const editJob = (req: Request, res: Response) => {
             job.updateOne(
                 job,
                 (err: any, raw: any)=> {
-                    
+
                     if (err) {
                         return res.json({'status': Status.Error, 'message': Messages.GenericError})
                     }
-    
+
                     return res.json({'status': Status.Success, 'message': 'Job edited successfully.'})
                 }
             )
-            
+
         }
     )
 }
@@ -557,9 +554,9 @@ export const getJobDetails = (req: Request, res: Response) => {
                 path: 'equipment',
                 select: 'info.model info.serialNumber info.nfcTag images info.location',
                 populate: [{ path: 'brand', select: 'title' },{ path: 'type', select: 'title' }],
-                
+
             })
-            
+
             const POPromise = PurchaseOrder.find({
                 job: params.jobId
             })
@@ -591,7 +588,7 @@ export const getJobReport = (req: Request, res: Response) => {
     if(req.otherCompanyId != undefined) {
         companyId = req.otherCompanyId
     }
-     
+
     Job.findOne({_id: params.jobId, company: companyId})
         .populate({
             path: 'ticket',
@@ -633,7 +630,7 @@ export const getJobReport = (req: Request, res: Response) => {
                 select: 'info.model info.serialNumber info.nfcTag images info.location',
                 populate: [{ path: 'brand', select: 'title' },{ path: 'type', select: 'title' }],
             })
-            
+
             const POPromise = PurchaseOrder.find({
                 job: params.jobId
             })
@@ -666,9 +663,9 @@ export const getTodaysJobsByTechnicianId = (req: Request, res: Response) => {
     date.setHours(0, 0, 0, 0)
     var endDate = new Date()
     endDate.setHours(23, 59, 59, 59)
-    
+
     const params = req.body
-    
+
     Job.find({ technician: params.employeeId, $and: [ { status: { $ne: 2 } }, { status: { $ne: 3 } } ], dateTime: {
         $gte: date,
         $lte: endDate
@@ -702,7 +699,7 @@ export const getTodaysJobsByTechnicianId = (req: Request, res: Response) => {
                 return res.json({'status': Status.Error, 'message': Messages.GenericError})
             }
 
-            return res.json({'status': Status.Success, 'jobs': jobs})    
+            return res.json({'status': Status.Success, 'jobs': jobs})
 
         }
     )
@@ -743,31 +740,31 @@ export const updateJobTime = (req: Request, res: Response) => {
     .then((result: any) => {
         const job = result[0]
         const item = result[1]
-        
+
         let timeSpent: number = 0
         let newcharges:  number = 0
         let startTime: Date = job.startTime
         let endTime: Date = job.startTime
-        
+
         if(params.startTime != undefined && params.startTime != null && params.startTime != '""') {
-            startTime = params.startTime    
+            startTime = params.startTime
         }
 
         if(params.endTime != undefined && params.endTime != null && params.endTime != '""') {
-            endTime = params.endTime    
+            endTime = params.endTime
         }
 
         if(item != null && item.charges > 0) {
-            
+
             if(params.status == 2 && !item.isFixed) {
                 let starting: any = new Date(startTime)
                 let ending: any = new Date(endTime)
-                
+
                 let diffMs = ( ending - starting);
                 let interval = 15 * 60 * 1000;
                 timeSpent = ((Math.ceil(diffMs / interval)*interval)/60000)/60
                 newcharges = item.charges * timeSpent
-            
+
             } else if(params.status == 2 && item.isFixed) {
                 newcharges = item.charges
             }
@@ -775,7 +772,7 @@ export const updateJobTime = (req: Request, res: Response) => {
 
         const user = <IUser>req.user
         return job.updateOne({ startTime: startTime, endTime: endTime, timeSpent: timeSpent, charges: newcharges, timeUpdatedBy: user._id, timeUpdatedAt: Date.now() })
-        
+
     })
     .then((response: any) => {
         return res.json({'status': Status.Success, 'message': 'Job time updated successfully.'})
