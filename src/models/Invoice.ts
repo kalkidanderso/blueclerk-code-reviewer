9b71af1a0abb26e51c56037a06d42288878bc258
Kristono Sugiarto <kristono.sugiarto@outlook.com>
2021-05-22 03:40:50 +0700

    [BLUECLERK-257] Invoice dueDate & Item isFixed
    - Add isFixed on each item to the Invoice
    
    - Add dueDate to the Invoice
    
    - Fix items.item populate data on getInvoiceDetail
    
    - Remove unused fields on Invoice
    
    - Mark the job's invoiceCreated as true when invoice created for that job
    
    - Refactor codes on Invoice Controller

--- src/models/Invoice.ts
+++ src/models/Invoice.ts
@@ -8,9 +8,10 @@ export interface IInvoice extends Document {
     purchaseOrder: Schema.Types.ObjectId
     estimate: Schema.Types.ObjectId
     jobPurchaseOrders: [Schema.Types.ObjectId]
+    dueDate: Date,
     customer: Schema.Types.ObjectId | any
     company: Schema.Types.ObjectId
-    note : String
+    note: string
     charges: number
     shippingCost: number
     tax: number
@@ -19,12 +20,14 @@ export interface IInvoice extends Document {
     createdBy: Schema.Types.ObjectId
     createdAt: Date,
     timeSpent: number
-    isFixed: boolean
-    hourlyRate: number
+    // isFixed: boolean
+    // hourlyRate: number
     items: [{
         item: Schema.Types.ObjectId
-        name: String
-        description: String
+        name: string
+        description: string
+        isFixed: boolean
+        hourlyRate: number
         price: number
         quantity: number
         tax: number
@@ -62,6 +65,9 @@ const InvoiceSchema = new Schema({
         ref: 'PurchaseOrder',
         required: false
     }],
+    dueDate: {
+        type: Date
+    },
     note: {
         type: String,
         required: false
@@ -108,14 +114,14 @@ const InvoiceSchema = new Schema({
         type: Number,
         default: 0
     },
-    isFixed: {
-        type: Boolean,
-        default: false
-    },
-    hourlyRate: {
-        type: Number,
-        default: 0
-    },
+    // isFixed: {
+    //     type: Boolean,
+    //     default: false
+    // },
+    // hourlyRate: {
+    //     type: Number,
+    //     default: 0
+    // },
     items: [{
         item: {
             type: Schema.Types.ObjectId,
@@ -137,6 +143,10 @@ const InvoiceSchema = new Schema({
         quantity: {
             type: Number,
             required: false
+        },
+        isFixed: {
+            type: Boolean,
+            default: true
         },
         tax: {
             type: Number,
