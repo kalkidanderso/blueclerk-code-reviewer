fe0186307d383765a252f23133ff552bb75c7869
Kristono Sugiarto <kristono.sugiarto@outlook.com>
2021-06-09 00:19:21 +0700

    [BLUECLERK-261] Update Swagger Dcumentation
    - Add IUser to the IPriceTier model's inactiveBy
    
    - Update Swagger description for Price Tier API
    
    - Update swagger.yaml accordingly

--- src/models/PriceTier.ts
+++ src/models/PriceTier.ts
@@ -1,4 +1,5 @@
 import mongoose, { Document, Schema } from 'mongoose';
+import { IUser } from './User';
 import { ICompany } from './Company';
 
 export interface IPriceTier extends Document {
@@ -6,7 +7,7 @@ export interface IPriceTier extends Document {
     companyId: Schema.Types.ObjectId | ICompany
     name: string
     isActive: boolean
-    inactiveBy?: Schema.Types.ObjectId
+    inactiveBy?: Schema.Types.ObjectId | IUser
     inactiveAt?: Date
 
 }
