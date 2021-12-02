0931e27bc12301455989563091f5001964a9c82a
avisanja <sanja.avi@outlook.com>
2021-12-02 13:36:51 +0700

    [BLUECLERK-491] Multiple Technicians Job
    - Update createInvoice API to be able save same jobType

--- src/controllers/invoice.ts
+++ src/controllers/invoice.ts
@@ -324,7 +324,14 @@ export const createInvoice = (req: Request, res: Response) => {
                 // Fallback for old job who still using one job type
                 if (!jobTypeIds.length) jobTypeIds.push(job.type);
                 // Search all jobTypes' items
-                const items = Item.find({ jobType: { $in: jobTypeIds }});
+                // const items = Item.find({ jobType: { $in: jobTypeIds }});
+                const items:any[] = []
+                jobTypeIds.forEach(async (jobTypeId) => {
+                    const item = await Item.findOne({ jobType: jobTypeId })
+                    items.push(item);
+                });
+
+                // const items = jobTypeIds.map(jobTypeId => Item.findOne({ jobType: jobTypeId }))
                 return Promise.all([result[0], result[1], items])
             })
             .then((result: any) => {
@@ -994,7 +1001,7 @@ const _populateInvoiceData = async (req: Request, res: Response, job: IJob, jobT
             total += subTotal;
         }
 
-    } else if (jobTypeitems.length > 0) {
+    } else if (jobTypeitems.length >= 0) {
 
         // Iterate all jobTypes' items and add all to invoice's items
         for (const jobTypeitem of jobTypeitems) {
