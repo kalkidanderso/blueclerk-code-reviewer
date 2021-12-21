a0b2f4f7821bc38b449ce8deeaec21fb8d6ff836
Kristono Sugiarto <kristono.sugiarto@outlook.com>
2021-12-22 00:50:17 +0700

    [BLUECLERK-413] Remove unused code
    - Remove unused code

--- src/controllers/quickbook.customer.ts
+++ src/controllers/quickbook.customer.ts
@@ -1,4 +1,4 @@
-import { Request, response, Response } from 'express'
+import { Request, Response } from 'express'
 import { Status, Messages, Role, NotificationTypes } from '../common/constants'
 
 import { IContact } from '../common/contact';
