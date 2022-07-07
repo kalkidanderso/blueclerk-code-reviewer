af9441b8b4d601c0a612593992ca01e4f33a072e
Kristono Sugiarto <kristono.sugiarto@outlook.com>
2022-07-07 15:48:10 +0700

    [BS-38] New Signup
    - Rename type to userType to avoid any unwanted case since type could be
    a reserved name
    
    - Refactor validator for signup API
    
    - Add more description to the signup API on swagger
    
    - Refactor login response to have token sorted on top, for easier use on
    Swagger
    
    - Update swagger.yaml

--- src/controllers/user.ts
+++ src/controllers/user.ts
@@ -60,7 +60,12 @@ export const login = (req: Request, res: Response, sio: any) => {
                                 return res.json({ 'status': Status.Error, 'message': Messages.InvalidEmailPassword })
                             }
 
-                            return res.json({ 'status': Status.Success, userType: user.permissions.role, 'user': user, 'token': user.jwt() })
+                            return res.json({
+                                status: Status.Success,
+                                token: user.jwt(),
+                                userType: user.permissions.role,
+                                user,
+                            });
                         })
                     })
 
@@ -91,7 +96,13 @@ export const login = (req: Request, res: Response, sio: any) => {
                             company.qbRefreshToken = undefined
                             company.socketId = undefined
                             company.realmId = undefined
-                            return res.json({ 'status': Status.Success, userType: user.permissions.role, 'user': user, 'company': company, 'token': user.jwt() })
+
+                            return res.json({
+                                status: Status.Success,
+                                token: user.jwt(),
+                                userType: user.permissions.role,
+                                user, company
+                            });
                         }
                     )
 
@@ -104,7 +115,12 @@ export const login = (req: Request, res: Response, sio: any) => {
                         return res.json({ 'status': Status.Error, 'message': Messages.InvalidEmailPassword })
                     }
 
-                    return res.json({ 'status': Status.Success, userType: user.permissions.role, 'user': user, 'token': user.jwt() })
+                    return res.json({
+                        status: Status.Success,
+                        token: user.jwt(),
+                        userType: user.permissions.role,
+                        user,
+                    });
                 })
             }
         }
@@ -198,15 +214,15 @@ export const signup = async (req: Request, res: Response, sio: any) => {
             info: params.email,
         }
 
-        switch (params.type) {
+        switch (params.userType) {
             case UserType.BUILDER:
                 if (!params.customerId) {
-                    return res.json({ status: Status.Error, message: 'customerId is required for this user type' })
+                    return res.json({ status: Status.Error, message: 'customerId is required for Builder userType' })
                 }
 
                 const customer = await Customer.findById(params.customerId);
                 if (!customer) {
-                    return res.json({ status: Status.NotFound, message: 'customer not found' })
+                    return res.json({ status: Status.Error, message: 'Customer not found' })
                 }
 
                 userEntry.customer = customer;
@@ -223,12 +239,12 @@ export const signup = async (req: Request, res: Response, sio: any) => {
 
             case UserType.SUPPLIER:
                 if (!params.companyId) {
-                    return res.json({ status: Status.Error, message: 'companyId is required on type supplier' })
+                    return res.json({ status: Status.Error, message: 'companyId is required for Supplier userType' })
                 }
 
                 const company = await Company.findById(params.companyId);
                 if (!company) {
-                    return res.json({ status: Status.NotFound, message: 'Company Not Found' })
+                    return res.json({ status: Status.Error, message: 'Company not found' })
                 }
 
                 req.company = company;
@@ -261,7 +277,7 @@ export const signup = async (req: Request, res: Response, sio: any) => {
                 const bcCompany = await Company.findById(BC_COMPANY_ID);
 
                 if (!bcCompany) {
-                    return res.json({ status: Status.NotFound, message: 'Company not found' });
+                    return res.json({ status: Status.Error, message: 'Company not found' });
                 }
 
                 userEntry.company = bcCompany;
@@ -278,7 +294,11 @@ export const signup = async (req: Request, res: Response, sio: any) => {
 
             case UserType.COMPANY:
             default:
+                if (!params.companyName) {
+                    return res.json({ status: Status.Error, message: 'companyName is required for Company Signup.' })
+                }
                 createCompany(req, res, sio);
+                break;
         }
 
     });
@@ -298,12 +318,12 @@ export const createCompany = (req: Request, res: Response, sio: any) => {
         }
 
         if (!params.industryId) {
-            return res.json({ status: Status.Error, message: 'industryId is required' });
+            return res.json({ status: Status.Error, message: 'industryId is required for Company Signup' });
         }
 
         const industry = await Industry.findById(params.industryId);
         if (!industry) {
-            return res.json({ status: Status.NotFound, message: 'indsutry not found' });
+            return res.json({ status: Status.NotFound, message: 'Industry not found' });
         }
 
         const company = new Company(
