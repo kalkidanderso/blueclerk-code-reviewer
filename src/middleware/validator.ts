9349dcf4fd70a268e9993ba9674bf793f2a9d5ee
Kristono Sugiarto <kristono.sugiarto@outlook.com>
2022-08-10 02:57:06 +0700

    [BC-699] Update payroll and record payment contractor
    - Update get payroll API to return the balance due for vendor and
    employee, advance payments and payments history
    
    - Update record payment contractor API to deduct vendor/employee credit
    
    - Update validation for record payment contractor API
    
    - Add new property creditUsed for PaymentVendor and PaymentEmployee
    
    - Update swagger accordingly

--- src/middleware/validator.ts
+++ src/middleware/validator.ts
@@ -523,6 +523,7 @@ export const Validations = {
     check('id').exists().withMessage(Messages.Required),
     check('id').isMongoId().withMessage(Messages.WrongId),
     check('amount').exists().withMessage(Messages.Required),
+    check('creditUsed').optional().isFloat().toFloat().withMessage('has to be number/decimal'),
   ],
 
   updatePaymentContractor: [
