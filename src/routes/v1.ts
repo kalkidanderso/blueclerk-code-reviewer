f38ca477936d1f64d5f6136afbc42c065b4681bc
Kristono Sugiarto <kristono.sugiarto@outlook.com>
2022-09-07 01:41:01 +0700

    Fix master pipeline issue
    - Revert changes for addEmailDefault script that don't have the model
    and constant supported

--- src/routes/v1.ts
+++ src/routes/v1.ts
@@ -2567,12 +2567,6 @@ export default function (sio: any) {
     )
 
     router.post(
-        '/script/addDefaultEmailTypes',
-        passport.authenticate('jwt', {session: false}),
-        scriptController.addDefaultEmailTypes
-    )
-
-    router.post(
         '/script/revertBackInvoices',
         scriptController.revertBackInvoices
     )
