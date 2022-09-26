3214d4367f359b9854025f0bab7af63f584db578
Kristono Sugiarto <kristono.sugiarto@outlook.com>
2022-09-27 01:51:27 +0700

    [BC-713] Fix Invoice Date PDF
    - Fix invoice's date PDF to use the invoice's issued date before the
    created at date

--- src/controllers/invoice.ts
+++ src/controllers/invoice.ts
@@ -2980,7 +2980,7 @@ export const _generateInvoicePdf = async (company: ICompany, invoice: IInvoice) 
                                 alignment: "right",
                             },
                             {
-                                text: `${moment(invoice.createdAt).format('MMM. DD, YYYY')}`,
+                                text: `${moment(invoice.issuedDate ?? invoice.createdAt).format('MMM. DD, YYYY')}`,
                                 style: "defaultFont",
                                 alignment: "right",
                             },
