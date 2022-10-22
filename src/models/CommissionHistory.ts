31e3ba966353fc3b491147e03c42a44919f531a0
Guston-era <me@gustonera.co.ke>
2022-10-22 05:42:38 +0300

    add commission history model

---
+++ src/models/CommissionHistory.ts
@@ -0,0 +1,49 @@
+import mongoose, { Document, Schema } from 'mongoose';
+
+
+export interface ICommissionHistory extends Document {
+    technicianOrContractor: string    
+    commission: number
+    editedBy: {
+        id: string
+        displayName: string
+    }
+    effectiveDate: Date
+}
+
+
+
+const CommissionHistorySchema = new Schema({
+
+    technicianOrContractor: {
+        type: Schema.Types.ObjectId,
+        required: true,
+        refPath: 'onCollection',
+    },       
+       
+    onCollection: {
+        type: String,
+        required: true,
+        enum: ['User', 'Company']
+    },
+
+     effectiveDate: {
+        type: Date,
+        default: null,
+    },
+     commission: {
+        type: Number,
+        default: null,
+    },
+    editedBy: {
+        id: {
+        type: Schema.Types.ObjectId,
+        ref: 'User',
+        },
+        displayName: String,
+    },
+   
+    
+}, { timestamps: { createdAt: true} })
+
+export const CommissionHistory = mongoose.model<ICommissionHistory>('CommissionHistory', CommissionHistorySchema)
