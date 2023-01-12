f66021c363edb4e9358b8202bc2b3cd26501bcb0
Sem Shekhovtsov <semshehovtsov@gmail.com>
2023-01-12 15:41:14 +0200

    added missing PO filter criteria to the search invoices api

--- src/controllers/invoice.ts
+++ src/controllers/invoice.ts
@@ -2421,6 +2421,9 @@ export const getInvoices = async (req: Request, res: Response) => {
         const customerPORegex = helper.getRegex(params.customerPO, 'i');
         filterQuery['$and'].push({ customerPO: customerPORegex });
     }
+    if (params.missingPO) {
+        filterQuery['$and'].push({ $or: [{ customerPO: '' }, { customerPO: null }]});
+    }
     if (params.customerId) {
         filterQuery['$and'].push({ customer: new ObjectId(params.customerId) });
     }
