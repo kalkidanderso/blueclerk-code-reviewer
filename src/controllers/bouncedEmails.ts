bfe2a6abe8e3655225659b68ef76a7955db5b75f
Hamid Ali <alihamidali2@gmail.com>
2023-07-10 14:45:14 +0500

    BL2-20: track email history delivery status individually

--- src/controllers/bouncedEmails.ts
+++ src/controllers/bouncedEmails.ts
@@ -1,6 +1,5 @@
 import { Request, Response } from 'express';
 import { Invoice } from '../models/Invoice';
-import { indexOf } from 'lodash';
 /**
  * To store bounced emails in db
  */
@@ -10,16 +9,21 @@ export const store = async (req: Request, res: Response) => {
         const { email } = req.body
         
         const invoice = await Invoice
-        .findOne(
-            { 'emailHistory.sentTo': email.trim()}
-        );
+        .findOneAndUpdate(
+            { 'emailHistory.sentTo': email.trim() },
+            { 
+                $set: { 
+                    'emailHistory.$.deliveryStatus': false,
+                    bouncedEmailFlag: true
+                },
+            },
+            { new: true }
 
+        );
+       
         if (!invoice) {
             res.status(500).json({message:'Invoice not found'})
         }
-
-        invoice.bouncedEmailFlag = true;
-        invoice.save()
 
         res.status(200).json({message: 'Success'})
 
