72ec20e9f866c493d6c5d6dff331bc584e885738
Daniel Díaz Lajara <ddl10@alu.ua.es>
2023-07-14 09:23:18 +0200

    feat: :sparkles: add homeowner edition logic on jobEdit endpoint
    Related issue BL2-156

--- src/controllers/job.ts
+++ src/controllers/job.ts
@@ -2992,13 +2992,29 @@ export const editJob = async (req: Request, res: Response) => {
                 }
 
                 job.isHomeOccupied = params.isHomeOccupied;
-                //job.jobLocation = null;
-                //job.jobSite = null;
                 if (linkedJob) { 
                     linkedJob.isHomeOccupied = params.isHomeOccupied; 
-                    //linkedJob.jobLocation = null; 
-                    //linkedJob.jobSite = null; 
                 }
+            }
+
+            if(params.isHomeOccupied === true) {
+                if(params.homeOwnerId && params.homeOwnerId !== job.homeOwner) {
+                    const newHomeOwner = await HomeOwner.findOne({ _id: params.homeOwnerId });
+                    if(!newHomeOwner) {
+                        return res.json({ 'status': Status.NotFound, 'message': 'Provided homeOwnerId does not correspond with any home owner' });
+                    }
+                    job.homeOwner = params.homeOwnerId;
+                    action  += '|Updated Home Owner|'
+                }
+                else {
+                    if(params.isHomeOccupied === true && !job.homeOwner) {
+                        return res.json({ 'status': Status.Error, 'message': 'Home Owner is required when home is occupied' });
+                    }
+                }
+            }
+            else if(params.isHomeOccupied === false) {
+                job.homeOwner = null;
+                action  += '|Updated Home Owner|'
             }
 
             if (params.customerContactId) {
