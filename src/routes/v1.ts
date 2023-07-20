66b61d5bc4ff3d77df3ae354ef714c141ee055a9
Usama Zulfiqar <usama.sama@gmail.com>
2023-07-20 16:29:20 +0500

    BL2-176: (add) un-void invoice api

--- src/routes/v1.ts
+++ src/routes/v1.ts
@@ -2075,7 +2075,15 @@ export default function (sio: any) {
         validate(Validations.companyInvoice),
         invoiceController.getCompanyInvoiceDetails
     )
-
+    router.post(
+        '/unvoidInvoice',
+        passport.authenticate('jwt', { session: false }),
+        isLogin(),
+        getCompanyId(),
+        checkUserPermissions(Permissions.Get_Invoices),
+        validate(Validations.unVoidInvoice),
+        invoiceController.unVoidInvoice
+    )
     router.delete(
         '/voidInvoice',
         passport.authenticate('jwt', { session: false }),
