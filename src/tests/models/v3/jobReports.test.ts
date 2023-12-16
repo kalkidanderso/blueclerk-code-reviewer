3f70fd35317ba86ea5557fc947246834634976d5
YN1 Chen <yu.w.chen1@gmail.com>
2023-12-16 15:17:42 -0800

    unit tests

---
+++ src/tests/models/v3/jobReports.test.ts
@@ -0,0 +1,183 @@
+import { JobReports } from '../../../models/v3/jobReports';
+import { ICreateJobReportsInput } from "../../../types/v3/jobReports";
+
+class CustomError extends Error {
+    code: string;
+    meta: {
+        field_name?: string;
+    };
+    constructor(fieldName: string, code: string, message?: string) {
+        super(message || '');
+        this.meta = { field_name: fieldName };
+        this.code = code;
+    }
+}
+
+let mockPrisma = {
+    create: jest.fn(),
+    update: jest.fn(),
+    findMany: jest.fn(),
+    findUnique: jest.fn(), 
+    delete: jest.fn(),     
+    count: jest.fn(), 
+};
+
+let jobReports: JobReports;
+
+beforeEach(() => {
+    jobReports = new JobReports(mockPrisma as any);
+});
+
+afterAll(() => {
+    jest.clearAllMocks();
+});
+
+// Sample input and return object
+const input: ICreateJobReportsInput = {
+    jobId: 123,
+    scanIds: [101, 102, 103],
+    customerName: "Acme Corp",
+    technicianName: "Jane Doe",
+    jobDate: new Date('2023-01-01'),
+    purchaseOrderIds: [301, 302],
+    companyId: 10,
+    contractorId: 20,
+    emailHistory: [
+        { date: new Date('2023-01-02'), email: "customer@example.com" },
+        { date: new Date('2023-01-03'), email: "followup@example.com" }
+    ],
+    lastEmailSent: new Date('2023-01-04'),
+    createdAt: new Date('2023-01-01'),
+    invoiceCreated: true,
+    invoiceVoid: false,
+    invoiceId: 400
+};
+
+const jobReportReturn = {
+    jobId: input.jobId,
+    scans: input.scanIds.map(id => ({ id })),
+    customerName: input.customerName,
+    technicianName: input.technicianName,
+    jobDate: input.jobDate,
+    purchaseOrders: input.purchaseOrderIds.map(id => ({ id })),
+    companyId: input.companyId,
+    contractorId: input.contractorId,
+    emailHistory: input.emailHistory,
+    lastEmailSent: input.lastEmailSent,
+    createdAt: input.createdAt,
+    invoiceCreated: input.invoiceCreated,
+    invoiceVoid: input.invoiceVoid,
+    invoiceId: input.invoiceId
+};
+
+describe('JobReports tests', () => {
+    // Creation of test report
+    it('create job report successfully', async () => {
+        mockPrisma.create = jest.fn().mockResolvedValue(input);
+        const result = await jobReports.create(input);
+        expect(mockPrisma.create).toHaveBeenCalled();
+        expect(result).toEqual(input);
+    });
+
+    it('create job report with database error', async () => {
+        mockPrisma.create = jest.fn(() => {
+            throw new CustomError('jobId', 'P2002', 'Unique constraint failed on the field: jobId');
+        });
+        try {
+            await jobReports.create(input);
+            fail('Test should have failed but it did not');
+        } catch (error) {
+            expect(mockPrisma.create).toHaveBeenCalled();
+            expect(error).toBeInstanceOf(Error);
+            expect(error.message).toContain('Unique constraint failed on the field: jobId');
+        }
+    });
+
+    // Creation of test update
+    it('update job report successfully', async () => {
+        mockPrisma.update = jest.fn().mockResolvedValue(updatedJobReport);
+        const result = await jobReports.updateById(1, input);
+        expect(mockPrisma.update).toHaveBeenCalled();
+        expect(result).toEqual(updatedJobReport);
+    });
+
+    it('update job report with database error', async () => {
+        mockPrisma.update = jest.fn(() => {
+            throw new CustomError('jobId', 'P2002', 'Constraint failed on the field: jobId');
+        });
+        try {
+            await jobReports.updateById(1, input);
+            fail('Test should have failed but it did not');
+        } catch (error) {
+            expect(mockPrisma.update).toHaveBeenCalled();
+            expect(error).toBeInstanceOf(Error);
+            expect(error.message).toContain('Constraint failed on the field: jobId');
+        }
+    });
+
+    //Test find report by ID
+    it('find job report by id successfully', async () => {
+        mockPrisma.findUnique = jest.fn().mockResolvedValue(foundJobReport);
+        const result = await jobReports.findById(1);
+        expect(mockPrisma.findUnique).toHaveBeenCalled();
+        expect(result).toEqual(foundJobReport);
+    });
+
+    it('find job report by id with database error', async () => {
+        mockPrisma.findUnique = jest.fn(() => {
+            throw new CustomError('id', 'P2025', 'Record not found.');
+        });
+        try {
+            await jobReports.findById(1);
+            fail('Test should have failed but it did not');
+        } catch (error) {
+            expect(mockPrisma.findUnique).toHaveBeenCalled();
+            expect(error).toBeInstanceOf(Error);
+            expect(error.message).toContain('Record not found.');
+        }
+    });
+
+    //Test to find the report by condition
+    it('find job reports successfully', async () => {
+        mockPrisma.findMany = jest.fn().mockResolvedValue([foundJobReport]);
+        const result = await jobReports.find(query);
+        expect(mockPrisma.findMany).toHaveBeenCalled();
+        expect(result).toEqual([foundJobReport]);
+    });
+
+    it('find job reports with database error', async () => {
+        mockPrisma.findMany = jest.fn(() => {
+            throw new CustomError('query', 'P2000', 'Query error');
+        });
+        try {
+            await jobReports.find(query);
+            fail('Test should have failed but it did not');
+        } catch (error) {
+            expect(mockPrisma.findMany).toHaveBeenCalled();
+            expect(error).toBeInstanceOf(Error);
+            expect(error.message).toContain('Query error');
+        }
+    });
+
+    //Test deleting report by ID
+    it('delete job report by id successfully', async () => {
+        mockPrisma.delete = jest.fn().mockResolvedValue(deletedJobReport);
+        const result = await jobReports.deleteById(1);
+        expect(mockPrisma.delete).toHaveBeenCalled();
+        expect(result).toEqual(deletedJobReport);
+    });
+
+    it('delete job report by id with database error', async () => {
+        mockPrisma.delete = jest.fn(() => {
+            throw new CustomError('id', 'P2025', 'Record to delete does not exist.');
+        });
+        try {
+            await jobReports.deleteById(1);
+            fail('Test should have failed but it did not');
+        } catch (error) {
+            expect(mockPrisma.delete).toHaveBeenCalled();
+            expect(error).toBeInstanceOf(Error);
+            expect(error.message).toContain('Record to delete does not exist.');
+        }
+    });
+});
---
+++ src/tests/services/v3/jobReports.test.ts
@@ -0,0 +1,201 @@
+import { JobReportServices } from '../../../services/v3/jobReports';
+import { JobReports } from "../../../models/v3/jobReports";
+import { PrismaClient } from "@prisma/client";
+import { Status, JobStatus } from "../../../common/constants";
+
+jest.mock('@prisma/client');
+jest.mock('../../models/v3/jobReports');
+
+describe('JobReportServices', () => {
+    let jobReportServices;
+    let mockJobReportsModel;
+    let mockPrisma;
+
+    beforeEach(() => {
+        mockPrisma = new PrismaClient();
+        mockJobReportsModel = new JobReports();
+        jobReportServices = new JobReportServices(mockPrisma);
+    });
+
+    afterEach(() => {
+        jest.clearAllMocks();
+    });
+
+    
+    describe('createJobReport', () => {
+        it('should throw an error if no job is found', async () => {
+            mockPrisma.job.findFirst.mockResolvedValue(null);
+
+            try {
+                await jobReportServices.createJobReport(1, 1, 'Customer Name', 'Technician Name', new Date());
+                throw new Error('Test failed, should have thrown an error');
+            } catch (error) {
+                expect(error.message).toBe('Job not found!');
+            }
+        });
+
+        it('should successfully create a job report', async () => {
+            mockPrisma.job.findFirst.mockResolvedValue({ id: 1, status: JobStatus.FINISHED });
+            mockJobReportsModel.deleteMany.mockResolvedValue(true);
+            mockJobReportsModel.create.mockResolvedValue({});
+
+            const response = await jobReportServices.createJobReport(1, 1, 'Customer Name', 'Technician Name', new Date());
+            expect(response.status).toBe(Status.Success);
+            expect(response.message).toBe('Job report created successfully');
+        });
+    });
+
+    describe('getAllJobReports', () => {
+        it('should throw an error when an unexpected error occurs', async () => {
+            // Setup mocks to simulate an error
+            mockJobReportsModel.findWithPagination.mockRejectedValue(new Error('Unexpected error'));
+            mockJobReportsModel.totalJobReports.mockResolvedValue(0);
+    
+            try {
+                await jobReportServices.getAllJobReports('company-id', {});
+                throw new Error('Test failed, should have thrown an error');
+            } catch (error) {
+                expect(error.message).toBe('Unexpected error');
+            }
+        });
+    
+        it('should successfully retrieve job reports with default filters', async () => {
+            // Setup mocks for successful retrieval
+            mockJobReportsModel.findWithPagination.mockResolvedValue([]);
+            mockJobReportsModel.totalJobReports.mockResolvedValue(10);
+    
+            const response = await jobReportServices.getAllJobReports('company-id', {});
+            expect(response.status).toBe(Status.Success);
+            expect(response.reports).toEqual([]);
+            expect(response.total).toBe(10);
+        });
+    
+        it('should successfully retrieve job reports with custom filters', async () => {
+            // Setup mocks for successful retrieval
+            const customFilters = { keyword: 'test', startDate: '2023-01-01', endDate: '2023-01-31', currentPage: '1', pageSize: '5' };
+            mockJobReportsModel.findWithPagination.mockResolvedValue([]);
+            mockJobReportsModel.totalJobReports.mockResolvedValue(5);
+    
+            const response = await jobReportServices.getAllJobReports('company-id', customFilters);
+            expect(response.status).toBe(Status.Success);
+            expect(response.reports).toEqual([]);
+            expect(response.total).toBe(5);
+        });
+    });
+
+    describe('getJobReportDetails', () => {
+        it('should throw an error if no report is found', async () => {
+            mockJobReportsModel.findById.mockResolvedValue(null);
+
+            try {
+                await jobReportServices.getJobReportDetails(1, 1);
+                throw new Error('Test failed, should have thrown an error');
+            } catch (error) {
+                expect(error.message).toBe('No report was found!');
+            }
+        });
+
+        it('should successfully retrieve job report details', async () => {
+            const mockReport = { id: 1, details: 'Some report details' };
+            mockJobReportsModel.findById.mockResolvedValue(mockReport);
+
+            const response = await jobReportServices.getJobReportDetails(1, 1);
+            expect(response.status).toBe(Status.Success);
+            expect(response.message).toEqual(mockReport);
+        });
+    });
+
+    describe('deleteJobReportById', () => {
+        it('should throw an error if no report is found or no permission to delete', async () => {
+            mockJobReportsModel.deleteById.mockResolvedValue({ count: 0 });
+
+            try {
+                await jobReportServices.deleteJobReportById(1, 1);
+                throw new Error('Test failed, should have thrown an error');
+            } catch (error) {
+                expect(error.message).toBe('No report found or you do not have permission to delete this report.');
+            }
+        });
+
+        it('should successfully delete a job report', async () => {
+            mockJobReportsModel.deleteById.mockResolvedValue({ count: 1 });
+
+            const response = await jobReportServices.deleteJobReportById(1, 1);
+            expect(response.status).toBe(Status.Success);
+            expect(response.message).toBe('Job Report has been deleted successfully!');
+        });
+    });
+    
+    // Tests for sendJobReport method
+    describe('sendJobReport', () => {
+        const jobReportId = 123;
+        const companyId = 456;
+        const user = { name: 'Test User' };
+
+        it('should throw an error if report is not found', async () => {
+            mockJobReportsModel.findById.mockResolvedValue(null);
+
+            try {
+                await jobReportServices.sendJobReport(jobReportId, companyId, user);
+                throw new Error('Test failed, should have thrown an error');
+            } catch (error) {
+                expect(error.message).toBe('Report was not found');
+            }
+        });
+
+        it('should successfully send a job report', async () => {
+            const report = { id: jobReportId, companyId: companyId };
+            mockJobReportsModel.findById.mockResolvedValue(report);
+            mockPrisma.jobReport.update.mockResolvedValue({ ...report, lastEmailSent: new Date() });
+
+            try {
+                const response = await jobReportServices.sendJobReport(jobReportId, companyId, user);
+                expect(response.status).toBe(Status.Success);
+                expect(response.message).toBe('Job Report has been sent successfully!');
+            } catch (error) {
+                throw new Error(`Unexpected error: ${error}`);
+            }
+        });
+    });
+
+    describe('getJobReportEmailTemplate', () => {
+        const validReportId = 123;
+        const companyId = 456;
+        const company = { info: { companyName: 'Test Company', companyEmail: 'test@test.com' } };
+
+        it('should throw an error if no job report is found', async () => {
+            mockJobReportsModel.findById.mockResolvedValue(null);
+
+            try {
+                await jobReportServices.getJobReportEmailTemplate(validReportId, companyId, company);
+                throw new Error('Test failed, should have thrown an error');
+            } catch (error) {
+                expect(error.message).toBe('Report was not found');
+            }
+        });
+
+        it('should successfully return an email template for a job report', async () => {
+            const jobReport = {
+                job: {
+                    tasks: [{ jobType: [{ title: 'Job Type 1', description: 'Description 1' }] }],
+                    customer: { info: { email: 'customer@test.com' }, profile: { displayName: 'Customer Name' } },
+                    jobLocation: { name: 'Location Name' },
+                    jobId: '1234',
+                },
+                jobDate: new Date('2023-01-01'),
+            };
+            mockJobReportsModel.findById.mockResolvedValue(jobReport);
+
+            const response = await jobReportServices.getJobReportEmailTemplate(validReportId, companyId, company);
+
+            expect(response.status).toBe(Status.Success);
+            expect(response.jobReport).toBe(jobReport);
+            expect(response.emailTemplate).toBeDefined();
+            expect(response.emailTemplate.from).toBe(company.info.companyEmail);
+            expect(response.emailTemplate.to).toBe(jobReport.job.customer.info.email);
+            expect(response.emailTemplate.subject).toContain(company.info.companyName);
+        });
+    });
+
+
+});
